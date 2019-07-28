8bb9249b4b904493de6e7fa268bff55be72c169c
darshitachavda <dcchavda97@gmail.com>
2019-07-28 11:37:30 +0530

    Add project files.

---
+++ source/Manga.Infrastructure/EntityFrameworkDataAccess/Migrations/20190728054355_InitialCreate1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Manga.Infrastructure.EntityFrameworkDataAccess.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Credit",
+                keyColumn: "Id",
+                keyValue: new Guid("f5117315-e789-491a-b662-958c37237f9b"),
+                column: "TransactionDate",
+                value: new DateTime(2019, 7, 28, 5, 43, 55, 248, DateTimeKind.Utc).AddTicks(8687));
+
+            migrationBuilder.UpdateData(
+                table: "Debit",
+                keyColumn: "Id",
+                keyValue: new Guid("3d6032df-7a3b-46e6-8706-be971e3d539f"),
+                column: "TransactionDate",
+                value: new DateTime(2019, 7, 28, 5, 43, 55, 249, DateTimeKind.Utc).AddTicks(240));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Credit",
+                keyColumn: "Id",
+                keyValue: new Guid("f5117315-e789-491a-b662-958c37237f9b"),
+                column: "TransactionDate",
+                value: new DateTime(2019, 7, 8, 6, 41, 36, 905, DateTimeKind.Utc).AddTicks(537));
+
+            migrationBuilder.UpdateData(
+                table: "Debit",
+                keyColumn: "Id",
+                keyValue: new Guid("3d6032df-7a3b-46e6-8706-be971e3d539f"),
+                column: "TransactionDate",
+                value: new DateTime(2019, 7, 8, 6, 41, 36, 905, DateTimeKind.Utc).AddTicks(2241));
+        }
+    }
+}
